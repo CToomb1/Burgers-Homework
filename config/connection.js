2d063ec1af8d98d6c919bb9050a43da76e875c30
Conor Toomb <conor.toomb114@gmail.com>
2020-03-05 18:31:16 -0500

    typo fixed

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 
-if (process.env,JSWDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSBS_URL);
 } else {
   connection = mysql.createConnection ({
