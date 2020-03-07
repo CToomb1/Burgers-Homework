f523c65677781440073bca4a57c1dcc6f305b998
Conor Toomb <conor.toomb114@gmail.com>
2020-03-07 09:48:09 -0500

    .env added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 const connection = require('./config/connection');
 const routes = require('./routes');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
